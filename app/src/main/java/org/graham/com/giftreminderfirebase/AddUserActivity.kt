30a5f304c45bd2a0e68665465ad70e55d3cb5070
graham4 <bline163@gmail.com>
2018-01-25 07:48:49 -0600

    Changes to show error in edit text fields when user is added a new user reminder.

--- app/src/main/java/org/graham/com/giftreminderfirebase/AddUserActivity.kt
+++ app/src/main/java/org/graham/com/giftreminderfirebase/AddUserActivity.kt
@@ -2,6 +2,7 @@ package org.graham.com.giftreminderfirebase
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.TextInputLayout
 import android.support.v7.app.AppCompatActivity
 import android.text.Editable
 import android.text.TextWatcher
@@ -12,31 +13,92 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import org.graham.com.giftreminderfirebase.models.Gift
 import org.graham.com.giftreminderfirebase.models.Person
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddUserActivity : AppCompatActivity() {
 
     private val TAG: String = "AddUserActivity"
     private var userUid: String = ""
 
+    private lateinit var userName: EditText
+    private lateinit var userNameTextInput: TextInputLayout
+    private lateinit var giftCost: EditText
+    private lateinit var giftCostTextInput: TextInputLayout
+    private lateinit var giftDescription: EditText
+    private lateinit var giftDescriptionTextInput: TextInputLayout
+    private lateinit var reminderDate: EditText
+    private lateinit var reminderDateTextInput: TextInputLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_user)
 
-        val userName = findViewById<EditText>(R.id.edittext_name)
-        val reminderDate = findViewById<EditText>(R.id.edittext_reminder_date)
-        val giftCost = findViewById<EditText>(R.id.edittext_cost)
-        val giftDescription = findViewById<EditText>(R.id.edittext_gift_description)
+        userName = findViewById<EditText>(R.id.edittext_name)
+        userNameTextInput = findViewById<TextInputLayout>(R.id.textInputLayout_user_name)
+        userName.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {}
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (count > 0) {
+                    userNameTextInput.error = null
+                }
+            }
+        })
+        reminderDate = findViewById<EditText>(R.id.edittext_reminder_date)
+        reminderDateTextInput = findViewById<TextInputLayout>(R.id.textInputLayout_reminder_date)
+        reminderDate.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {}
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (count > 0) {
+                    reminderDateTextInput.error = null
+                }
+            }
+        })
+        giftCost = findViewById<EditText>(R.id.edittext_cost)
+        giftCostTextInput = findViewById<TextInputLayout>(R.id.textInputLayout_gift_cost)
+        giftCost.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {}
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (count > 0) {
+                    giftCostTextInput.error = null
+                }
+            }
+        })
+        giftDescription = findViewById<EditText>(R.id.edittext_gift_description)
+        giftDescriptionTextInput = findViewById<TextInputLayout>(R.id.textInputLayout_gift_description)
+        giftDescription.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {}
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (count > 0) {
+                    giftDescriptionTextInput.error = null
+                }
+            }
+        })
 
-        userName.onTextChanged { it }
-        reminderDate.onTextChanged { it }
-        giftCost.onTextChanged { it }
-        giftDescription.onTextChanged { it }
 
         val submitButton = findViewById<Button>(R.id.button_submit)
         submitButton.setOnClickListener { view ->
+            // Check if user entered proper fields
+            if (userName.text.isBlank()) {
+                userNameTextInput.error = "Error, please enter name."
+                return@setOnClickListener
+            } else if (reminderDate.text.isBlank()) {
+                reminderDateTextInput.error = "Error, please enter reminder date."
+                return@setOnClickListener
+            } else if (giftDescription.text.isBlank()) {
+                giftDescriptionTextInput.error = "Error, please enter description."
+                return@setOnClickListener
+            } else if (giftCost.text.isBlank()) {
+                giftCostTextInput.error = "Error, please enter gift cost."
+                return@setOnClickListener
+            }
 
-            //TODO: Update date of purchase
-            val gift = Gift(giftDescription.text.toString(), giftCost.text.toString(), "08/30/75")
+            val dateOfPurchanse: String = SimpleDateFormat(Constants.PURCHASE_DATE_FORMAT, Locale.US).format(Date())
+            val gift = Gift(giftDescription.text.toString(), giftCost.text.toString(), dateOfPurchanse)
             val person = Person(userName.text.toString(), reminderDate.text.toString(), gift)
 
             if(!userUid.isNullOrEmpty()) {
@@ -54,16 +116,5 @@ class AddUserActivity : AppCompatActivity() {
     private fun launchMainActivity() {
         val mainIntent = Intent(this, MainActivity::class.java)
         startActivity(mainIntent)
-    }
-
-    fun EditText.onTextChanged(cb: (String) -> Unit) {
-        this.addTextChangedListener(object: TextWatcher {
-            override fun afterTextChanged(s: Editable?) { }
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (cb.toString().isNotEmpty())
-                    error = null
-            }
-        })
     }
 }
